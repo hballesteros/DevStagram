1403d7e0b315c8b65f4c70393019c450ddcf01e2
Hugo <hugo@boahstudio.com>
2023-10-04 09:13:31 -0300

    Logout Controller

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,10 +14,25 @@
                     DevStagram
                 </h1>
 
-                <nav class="flex gap-2 items-center">
-                    <a class="font-bold uppercase text-gray-600 text-sm" href="{{ route('login') }}">Login</a>
-                    <a class="font-bold uppercase text-gray-600 text-sm" href="{{ route('register') }}">Crear Cuenta </a
-                </nav>
+                @auth
+                    <nav class="flex gap-2 items-center">
+                        <a class="font-bold text-gray-600 text-sm" href="#">
+                            Hola: <span class="font-normal">{{ auth()->user()->username }}</span>
+                        </a>
+                        <form action="{{ route('logout') }}" method="POST">
+                            @csrf
+                            <button class="font-bold uppercase text-gray-600 text-sm" type="submit">Cerrar Sesión</button>
+                        </form>
+                    </nav>
+                @endauth
+                
+                @guest
+                    <nav class="flex gap-2 items-center">
+                        <a class="font-bold uppercase text-gray-600 text-sm" href="{{ route('login') }}">Login</a>
+                        <a class="font-bold uppercase text-gray-600 text-sm" href="{{ route('register') }}">Crear Cuenta </a
+                    </nav>
+                @endguest
+
             </div>
         </header>
 
